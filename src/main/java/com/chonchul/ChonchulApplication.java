9bd44a07285aa36b9c0350f21982bc56e578365f
westzeroright <kt8267@naver.com>
2024-11-19 04:15:36 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/chonchul/ChonchulApplication.java
@@ -0,0 +1,13 @@
+package com.chonchul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChonchulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChonchulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chonchul/ChonchulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chonchul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChonchulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
