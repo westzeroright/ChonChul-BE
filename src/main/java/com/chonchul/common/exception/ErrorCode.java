19498088d847b69ba9e6cd67211ab714b866ed9e
westzeroright <kt8267@naver.com>
2024-11-26 21:45:24 +0900

    feat: 공통으로 사용할 예외처리

---
+++ src/main/java/com/chonchul/common/exception/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.chonchul.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String getCode();
+    HttpStatus getHttpStatus();
+    String getMessage();
+}
