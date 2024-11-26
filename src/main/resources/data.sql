a31f469ee1a90aa95487a7c2b54b44c8eff718ee
westzeroright <kt8267@naver.com>
2024-11-27 00:53:51 +0900

    refactor: 재설계된 테이블에 맞추어 목데이터 재삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,15 @@
-INSERT IGNORE INTO student (id, number, name, department, phone_number, email) VALUES (1, 221965, '서영우', '자연과학대학 수학과', '010-7731-3160', 'me031007@jnu.ac.kr');
+INSERT IGNORE INTO user (id, number, name, department, phone_number, email, role) VALUES (1, 221965, '서영우', '자연과학대학 수학과', '010-7731-3160', 'me031007@jnu.ac.kr', 'STUDENT');
 
-INSERT IGNORE INTO teacher (id, name) VALUES (1, '김명진'), (2, '조종택'), (3, '권도용');
+INSERT IGNORE INTO user (id, name, role) VALUES (2,'김명진','TEACHER'), (3,'조종택','TEACHER'),(4,'권도용','TEACHER');
+
+INSERT IGNORE INTO student (id) VALUES (1);
+
+INSERT IGNORE INTO teacher (id) VALUES (2),(3),(4);
 
 INSERT IGNORE INTO lecture (id, name, code, place, teacher_id) VALUES
-                                                            (1, '임베디드소프트웨어', 'ECE3088-2', '공7-219',1),
-                                                            (2, '미분기하1및실습', 'MTH3012-1', '자1-437',2),
-                                                            (3, '암호학입문', 'MTH4027-1', '자1-301',3);
+                                                            (1, '임베디드소프트웨어', 'ECE3088-2', '공7-219',2),
+                                                            (2, '미분기하1및실습', 'MTH3012-1', '자1-437',3),
+                                                            (3, '암호학입문', 'MTH4027-1', '자1-301',4);
 
 INSERT IGNORE INTO student_lecture (id, student_id, lecture_id) VALUES
                                                              (1,1,1),
