2382fc23cc315625cd0630cf8d88eea6787285f2
westzeroright <kt8267@naver.com>
2024-11-29 21:47:38 +0900

    feat: 회원 정보 입력에 대하여 유효성 검증

--- src/main/java/com/chonchul/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/chonchul/common/exception/GlobalExceptionHandler.java
@@ -2,8 +2,14 @@ package com.chonchul.common.exception;
 
 import com.chonchul.common.response.FailureBody;
 import com.chonchul.common.response.ResponseEntityGenerator;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
@@ -12,5 +18,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<FailureBody> handleConflict(BusinessException e) {
         return ResponseEntityGenerator.fail(e.getErrorCode().getCode(), e.getErrorCode().getMessage(), e.getErrorCode().getHttpStatus());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ResponseEntity<FailureBody> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        return ResponseEntityGenerator.fail(
+                e.getBindingResult().getFieldErrors().get(0).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/chonchul/user/presentation/UserController.java
+++ src/main/java/com/chonchul/user/presentation/UserController.java
@@ -4,8 +4,10 @@ import com.chonchul.common.response.ResponseEntityGenerator;
 import com.chonchul.common.response.SuccessBody;
 import com.chonchul.user.application.dto.UserInfoDto;
 import com.chonchul.user.application.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,13 +26,14 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<SuccessBody<UserInfoDto>> getUser(@PathVariable Long userId) {
+    public ResponseEntity<SuccessBody<UserInfoDto>> getUser(@PathVariable Long userId, Model model) {
         UserInfoDto userInfoDto = userService.findUser(userId);
+        model.addAttribute("hello", "서버에서 보내준 값입니다");
         return ResponseEntityGenerator.success(userInfoDto, "회원 조회 성공", HttpStatus.OK);
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<SuccessBody<Void>> updateUser(@PathVariable Long userId, @RequestBody UserInfoDto userInfoDto) {
+    public ResponseEntity<SuccessBody<Void>> updateUser(@PathVariable Long userId, @Valid @RequestBody UserInfoDto userInfoDto) {
         userService.modifyUser(userId,userInfoDto);
         return ResponseEntityGenerator.success(null,"회원 수정 성공", HttpStatus.OK);
     }
