93ffea808d02229ed31122d6183b6d518d7ff424
westzeroright <kt8267@naver.com>
2024-12-01 00:23:08 +0900

    refactor: 공통 응답 형식 사용

--- src/main/java/com/chonchul/lecture/presentation/LectureController.java
+++ src/main/java/com/chonchul/lecture/presentation/LectureController.java
@@ -1,11 +1,15 @@
 package com.chonchul.lecture.presentation;
 
+import com.chonchul.common.response.ResponseEntityGenerator;
+import com.chonchul.common.response.SuccessBody;
 import com.chonchul.lecture.application.dto.LectureInfoDto;
 import com.chonchul.lecture.application.dto.SessionInfoDto;
 import com.chonchul.lecture.application.service.LectureService;
 import com.chonchul.lecture.application.service.SessionService;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,17 +29,20 @@ public class LectureController {
     }
 
     @GetMapping("/{studentId}")
-    public List<LectureInfoDto> getLectures(@PathVariable Long studentId) {
-        return lectureService.getLectures(studentId);
+    public ResponseEntity<SuccessBody<List<LectureInfoDto>>> getLectures(@PathVariable Long studentId) {
+        List<LectureInfoDto> lectures = lectureService.getLectures(studentId);
+        return ResponseEntityGenerator.success(lectures, "수업 목록 조회 성공", HttpStatus.OK);
     }
 
     @GetMapping("/info/{lectureId}")
-    public LectureInfoDto getLectureInfo(@PathVariable Long lectureId) {
-        return lectureService.getLectureInfo(lectureId);
+    public ResponseEntity<SuccessBody<LectureInfoDto>> getLectureInfo(@PathVariable Long lectureId) {
+        LectureInfoDto lectureInfoDto =  lectureService.getLectureInfo(lectureId);
+        return ResponseEntityGenerator.success(lectureInfoDto, "수업 목록 조회 성공", HttpStatus.OK);
     }
 
     @GetMapping("/session")
-    public SessionInfoDto getLectureSession(@RequestParam LocalDate localDate, @RequestParam Long lectureId) {
-        return sessionService.getLectureSession(localDate,lectureId);
+    public ResponseEntity<SuccessBody<SessionInfoDto>> getLectureSession(@RequestParam LocalDate localDate, @RequestParam Long lectureId) {
+        SessionInfoDto sessionInfoDto = sessionService.getLectureSession(localDate, lectureId);
+        return ResponseEntityGenerator.success(sessionInfoDto,"강의주차 조회 성공", HttpStatus.OK);
     }
 }
