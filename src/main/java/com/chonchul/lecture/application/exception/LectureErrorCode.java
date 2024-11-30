637235ad886d4243b48a1a59cf13e644567a32e1
westzeroright <kt8267@naver.com>
2024-11-30 20:51:55 +0900

    feat: 강의를 조회할 수 없는 예외 상황

---
+++ src/main/java/com/chonchul/lecture/application/exception/LectureErrorCode.java
@@ -0,0 +1,33 @@
+package com.chonchul.lecture.application.exception;
+
+import com.chonchul.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum LectureErrorCode implements ErrorCode {
+    NOT_FOUND_LECTURE("2001", HttpStatus.NOT_FOUND,"강의를 찾을 수 없습니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    LectureErrorCode(String code, HttpStatus httpStatus, String message) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
