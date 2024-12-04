57873609583b6671587a599ab53481b60f053009
westzeroright <kt8267@naver.com>
2024-12-04 14:49:32 +0900

    feat: cors 설정

--- src/main/java/com/chonchul/config/WebMvcConfig.java
+++ src/main/java/com/chonchul/config/WebMvcConfig.java
@@ -4,18 +4,30 @@ import com.chonchul.auth.LoginUserArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginMemberArgumentResolver;
 
-    public WebConfig(LoginUserArgumentResolver loginMemberArgumentResolver) {
+    public WebMvcConfig(LoginUserArgumentResolver loginMemberArgumentResolver) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .maxAge(3600);
     }
 }
