641fd684ea6179353e39582870b16e2bd9915394
westzeroright <kt8267@naver.com>
2024-12-05 14:35:19 +0900

    feat: JwtInterceptor 적용

---
+++ src/main/java/com/chonchul/auth/JwtInterceptor.java
@@ -0,0 +1,66 @@
+package com.chonchul.auth;
+
+import com.chonchul.auth.token.TokenResolver;
+import com.chonchul.user.application.exception.NotFoundUserException;
+import com.chonchul.user.persistence.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+    private final TokenResolver tokenResolver;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+
+        logRequestDetails(request);
+
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = authorization.replaceAll("Bearer ", "");
+
+        if (token != null && token.length() > 10) {
+
+            if (vaildToken(token)) {
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                String member = objectMapper.writeValueAsString(tokenResolver.getUserDataByAccessToken(token));
+                User accessMember = objectMapper.readValue(member, User.class);
+
+                request.setAttribute("member", accessMember);
+                Long memberId = tokenResolver.getUserDataByAccessToken(token);
+                return true;
+            }
+        } else {
+            throw new NotFoundUserException();
+        }
+        return false;
+    }
+
+    private static void logRequestDetails(HttpServletRequest request) {
+        String clientIp = request.getHeader("X-Forwarded-For");
+        if (clientIp == null || clientIp.isEmpty()) {
+            clientIp = request.getRemoteAddr();
+        }
+    }
+
+    private boolean vaildToken(String token) {
+        try {
+            tokenResolver.getUserDataByAccessToken(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            throw new RuntimeException();
+        }
+    }
+}
--- src/main/java/com/chonchul/config/WebMvcConfig.java
+++ src/main/java/com/chonchul/config/WebMvcConfig.java
@@ -1,19 +1,20 @@
 package com.chonchul.config;
 
+import com.chonchul.auth.JwtInterceptor;
 import com.chonchul.auth.LoginUserArgumentResolver;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginMemberArgumentResolver;
-
-    public WebMvcConfig(LoginUserArgumentResolver loginMemberArgumentResolver) {
-        this.loginMemberArgumentResolver = loginMemberArgumentResolver;
-    }
+    private final JwtInterceptor jwtInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
@@ -29,5 +30,25 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        // Auth 관련 경로
+//                        "/lecute",
+
+                        // Swagger UI 관련 경로
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**",
+                        "/swagger/**",
+                        "/favicon.ico",
+                        "/error");
     }
 }
