29985a3e73508a5824adf851bf779f1bfc7e4d10
westzeroright <kt8267@naver.com>
2024-12-09 05:10:04 +0900

    feat: 목데이터 세팅
    - 학생 한명 회원가입시키고
    - 맞게 수업 등등 데이터 삽입
    - BaseEntity 적용

--- src/main/java/com/chonchul/ChonchulApplication.java
+++ src/main/java/com/chonchul/ChonchulApplication.java
@@ -2,8 +2,10 @@ package com.chonchul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChonchulApplication {
 
     public static void main(String[] args) {
